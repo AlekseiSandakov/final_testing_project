f603bca409ae5839018dd6298fef575580344019
axe455990 <axe455990@mail.ru>
2024-04-08 18:24:17 +0700

    encapsulation of business logic in methods

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,14 +9,12 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self, substring, full_string):
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
         substring = 'login'
-        full_string = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        assert substring in full_string, f"substring '{substring}' is not part of string '{full_string}'"
+        assert substring in current_url, f"substring '{substring}' is not part of string '{current_url}'"
 
     def should_be_login_form(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
